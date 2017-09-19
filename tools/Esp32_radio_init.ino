def50841382a075efef29cb8c0e95da3b038421d
Ed Smallenburg <ed@smallenburg.nl>
2017-09-18 17:42:08 -0700

    Update Esp32_radio_init.ino

--- tools/Esp32_radio_init.ino
+++ tools/Esp32_radio_init.ino
@@ -36,8 +36,8 @@ void setup()
   preferences.putString ( "mqttpasswd",   "none" ) |
   preferences.putString ( "mqtttpreset",  "none" ) |
   //
-  preferences.putString ( "wifi_00", "NETGEAR-11/DEADC0DE11" ) |
-  preferences.putString ( "wifi_01", "ADSL-11/DEADC0DE11" ) |
+  preferences.putString ( "wifi_00", "ssid1/pw1" ) |
+  preferences.putString ( "wifi_01", "ssid2/pw2" ) |
   //
   preferences.putString ( "volume", "72" ) |
   preferences.putString ( "toneha", "0" ) |
